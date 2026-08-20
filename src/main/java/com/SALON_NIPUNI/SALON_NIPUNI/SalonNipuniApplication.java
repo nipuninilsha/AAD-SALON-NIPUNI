76ce0e2011e54b6ad983020abcb257a8cee9ed09
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-08-20 13:25:35 +0530

    Initial commit - Salon Nipuni project setup

---
+++ src/main/java/com/SALON_NIPUNI/SALON_NIPUNI/SalonNipuniApplication.java
@@ -0,0 +1,13 @@
+package com.SALON_NIPUNI.SALON_NIPUNI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonNipuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonNipuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SALON_NIPUNI/SALON_NIPUNI/SalonNipuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SALON_NIPUNI.SALON_NIPUNI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonNipuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
