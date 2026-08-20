75c6c09950770057d80e4f4c69be40cbcbef414f
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-08-20 17:36:34 +0530

    Add User entity skeleton with primary key

--- src/main/java/com/SALON_NIPUNI/SALON_NIPUNI/entity/User.java
+++ src/main/java/com/SALON_NIPUNI/SALON_NIPUNI/entity/User.java
@@ -1,4 +1,19 @@
 package com.SALON_NIPUNI.SALON_NIPUNI.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_user")
 public class User {
+
+    @Id
+    private String id;
 }
