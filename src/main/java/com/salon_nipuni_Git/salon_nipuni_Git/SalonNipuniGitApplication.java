df90bca0e4a5d7a5ff41a60a1e3ae25953fc6521
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-09 07:15:48 +0530

    Initial commit - salon nipuni git project

---
+++ src/main/java/com/salon_nipuni_Git/salon_nipuni_Git/SalonNipuniGitApplication.java
@@ -0,0 +1,13 @@
+package com.salon_nipuni_Git.salon_nipuni_Git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonNipuniGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonNipuniGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salon_nipuni_Git/salon_nipuni_Git/SalonNipuniGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salon_nipuni_Git.salon_nipuni_Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonNipuniGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
