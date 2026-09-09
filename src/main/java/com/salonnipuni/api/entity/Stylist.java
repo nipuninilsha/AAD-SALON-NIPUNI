a5cb302ade4a0cf7824dc257206acd94b901b7da
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 01:12:50 +0530

    feat: add Stylist entity

--- src/main/java/com/salonnipuni/api/entity/Stylist.java
+++ src/main/java/com/salonnipuni/api/entity/Stylist.java
@@ -1,4 +1,46 @@
 package com.salonnipuni.api.entity;
 
+import com.salonnipuni.api.enumeration.RecordStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "stylist")
 public class Stylist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private String contact;
+
+    private String address;
+
+    private String email;
+
+    private String specialty;
+
+    @jakarta.persistence.Column(length = 1000)
+    private String bio;
+
+    @jakarta.persistence.Column(length = 1000)
+    private String photo;
+
+    private String assignedServiceCategories;
+
+    @Enumerated(EnumType.STRING)
+    private RecordStatus status = RecordStatus.ACTIVE;
 }
