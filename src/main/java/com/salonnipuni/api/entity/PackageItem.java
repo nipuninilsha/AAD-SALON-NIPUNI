580c04367231594d624dd1c24c1512fe04f3dc68
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 01:07:53 +0530

    feat: add PackageItem entity

--- src/main/java/com/salonnipuni/api/entity/PackageItem.java
+++ src/main/java/com/salonnipuni/api/entity/PackageItem.java
@@ -1,4 +1,33 @@
 package com.salonnipuni.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class PackageItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String salonServiceId;
+
+    private String salonServiceName;
+
+    private int qty;
+
+    @ManyToOne
+    @JoinColumn(name = "package_id")
+    @JsonIgnore
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private BridalPackage bridalPackage;
 }
