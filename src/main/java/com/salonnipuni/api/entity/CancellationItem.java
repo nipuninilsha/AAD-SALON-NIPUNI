7ee07c5b810a25a40ff514bb5e8c4e5b27e0387b
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 01:00:43 +0530

    feat: add CancellationItem entity

--- src/main/java/com/salonnipuni/api/entity/CancellationItem.java
+++ src/main/java/com/salonnipuni/api/entity/CancellationItem.java
@@ -1,4 +1,36 @@
 package com.salonnipuni.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cancellation_item")
 public class CancellationItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String salonServiceId;
+
+    private String name;
+
+    private int qty;
+
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "booking_cancellation_id")
+    @JsonIgnore
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private BookingCancellation bookingCancellation;
 }
