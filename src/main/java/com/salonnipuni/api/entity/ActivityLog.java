eff67326dc3c8ef5f919727208621337c4e9d49e
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-09 18:50:08 +0530

    feat: add ActivityLog entity model

--- src/main/java/com/salonnipuni/api/entity/ActivityLog.java
+++ src/main/java/com/salonnipuni/api/entity/ActivityLog.java
@@ -1,4 +1,29 @@
 package com.salonnipuni.api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "activity_log")
 public class ActivityLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String action;
+
+    @Column(length = 500)
+    private String details;
+
+    private LocalDateTime timestamp;
 }
