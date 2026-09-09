b1259472608fc5cfb2b609a1e6400d1e2156ff98
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 00:46:25 +0530

    feat: add BookingItem entity

--- src/main/java/com/salonnipuni/api/entity/BookingItem.java
+++ src/main/java/com/salonnipuni/api/entity/BookingItem.java
@@ -1,4 +1,36 @@
 package com.salonnipuni.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "booking_item")
 public class BookingItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String salonServiceId;
+
+    private String name;
+
+    private int qty;
+
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "booking_id")
+    @JsonIgnore
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Booking booking;
 }
