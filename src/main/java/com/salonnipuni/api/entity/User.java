3785a5ab164eb8b3fa5f90f757cce4deba059282
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 01:14:33 +0530

    feat: add User entity

--- src/main/java/com/salonnipuni/api/entity/User.java
+++ src/main/java/com/salonnipuni/api/entity/User.java
@@ -1,4 +1,43 @@
 package com.salonnipuni.api.entity;
 
+import com.salonnipuni.api.enumeration.RecordStatus;
+import com.salonnipuni.api.enumeration.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "app_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private RecordStatus status = RecordStatus.ACTIVE;
+
+    private boolean forcePasswordChange = false;
+
+    private boolean twoFactorEnabled = false;
+
+    private String otpCode;
+
+    private java.time.LocalDateTime otpExpiry;
 }
