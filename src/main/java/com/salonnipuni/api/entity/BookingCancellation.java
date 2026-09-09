5c9e5060e7ef2288dc881c593f61e4a71e169227
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 00:25:18 +0530

    feat: add BookingCancellation entity

--- src/main/java/com/salonnipuni/api/entity/BookingCancellation.java
+++ src/main/java/com/salonnipuni/api/entity/BookingCancellation.java
@@ -1,4 +1,35 @@
 package com.salonnipuni.api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "booking_cancellation")
 public class BookingCancellation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String bookingId;
+
+    private String clientId;
+
+    private String reason;
+
+    private LocalDate date;
+
+    private double totalRefund;
+
+    @OneToMany(mappedBy = "bookingCancellation", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<CancellationItem> salonServices = new ArrayList<>();
 }
