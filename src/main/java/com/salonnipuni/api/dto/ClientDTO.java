489af0df2ad960cb7de7ef5d3081542921d154b9
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:50:17 +0530

    feat(dto): add ClientDTO class

--- src/main/java/com/salonnipuni/api/dto/ClientDTO.java
+++ src/main/java/com/salonnipuni/api/dto/ClientDTO.java
@@ -1,4 +1,28 @@
 package com.salonnipuni.api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientDTO {
+    private String id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Contact number is required")
+    private String contact;
+
+    private String address;
+
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    private Integer points;
+    private String tier;
 }
