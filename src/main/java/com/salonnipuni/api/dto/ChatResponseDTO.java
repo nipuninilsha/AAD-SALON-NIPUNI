bc2e6c70e941f40f6ce45caa607dde3f50b70a7a
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:50:06 +0530

    feat(dto): add ChatResponseDTO class

--- src/main/java/com/salonnipuni/api/dto/ChatResponseDTO.java
+++ src/main/java/com/salonnipuni/api/dto/ChatResponseDTO.java
@@ -1,4 +1,13 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatResponseDTO {
+    private String sessionId;
+    private String reply;
 }
