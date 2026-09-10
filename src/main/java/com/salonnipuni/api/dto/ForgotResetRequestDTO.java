0353a96a1582692340690be2c41f9072e84c1ae1
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:51:26 +0530

    feat(dto): add ForgotResetRequestDTO class

--- src/main/java/com/salonnipuni/api/dto/ForgotResetRequestDTO.java
+++ src/main/java/com/salonnipuni/api/dto/ForgotResetRequestDTO.java
@@ -1,4 +1,22 @@
 package com.salonnipuni.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ForgotResetRequestDTO {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Verification code is required")
+    private String otp;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 4, message = "New password must be at least 4 characters")
+    private String newPassword;
 }
