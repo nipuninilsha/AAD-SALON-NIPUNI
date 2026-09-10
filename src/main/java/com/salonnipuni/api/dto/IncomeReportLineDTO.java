ebc92d8ce4985e2b0a2bee2622af14a2f883ffc6
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:52:16 +0530

    feat(dto): add IncomeReportLineDTO class

--- src/main/java/com/salonnipuni/api/dto/IncomeReportLineDTO.java
+++ src/main/java/com/salonnipuni/api/dto/IncomeReportLineDTO.java
@@ -1,4 +1,17 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IncomeReportLineDTO {
+    private String date;
+    private String bookingId;
+    private String clientName;
+    private String paymentMethod;
+    private double amount;
+    private String type;
 }
