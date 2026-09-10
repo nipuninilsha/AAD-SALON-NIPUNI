8844804641190bca9fbf9245197a8b1784855c6d
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:53:12 +0530

    feat(dto): add SalonExpenseDTO class

--- src/main/java/com/salonnipuni/api/dto/SalonExpenseDTO.java
+++ src/main/java/com/salonnipuni/api/dto/SalonExpenseDTO.java
@@ -1,4 +1,19 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SalonExpenseDTO {
+    private String id;
+    private String category;
+    private String description;
+    private double amount;
+    private LocalDate date;
+    private String addedBy;
 }
