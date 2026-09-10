7b6058a913a8d4675a60d6a6e315a0ac33ba6ec5
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:52:27 +0530

    feat(dto): add PackageItemDTO class

--- src/main/java/com/salonnipuni/api/dto/PackageItemDTO.java
+++ src/main/java/com/salonnipuni/api/dto/PackageItemDTO.java
@@ -1,4 +1,14 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PackageItemDTO {
+    private String salonServiceId;
+    private String salonServiceName;
+    private int qty;
 }
