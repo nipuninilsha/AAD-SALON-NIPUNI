1a8ef8aa3ab5e5751fcaf215c033a6c9f2ef7a70
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:48:22 +0530

    feat(dto): add BridalPackageDTO class

--- src/main/java/com/salonnipuni/api/dto/BridalPackageDTO.java
+++ src/main/java/com/salonnipuni/api/dto/BridalPackageDTO.java
@@ -1,4 +1,20 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BridalPackageDTO {
+    private String id;
+    private String name;
+    private double price;
+    private String tier;
+    private String description;
+    private String image;
+    private List<PackageItemDTO> salonServices;
 }
