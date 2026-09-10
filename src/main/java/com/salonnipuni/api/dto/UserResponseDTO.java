83ddd909471100fd5467b6d298f6904b1d7d1ccb
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:54:50 +0530

    feat(dto): add UserResponseDTO class

--- src/main/java/com/salonnipuni/api/dto/UserResponseDTO.java
+++ src/main/java/com/salonnipuni/api/dto/UserResponseDTO.java
@@ -1,4 +1,17 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDTO {
+    private String id;
+    private String username;
+    private String name;
+    private String role;
+    private String email;
+    private boolean active;
 }
