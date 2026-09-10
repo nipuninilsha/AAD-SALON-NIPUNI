ea649542412fc51a0b424a34d9548b32741ee2e3
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:54:32 +0530

    feat(dto): add UserRequestDTO class

--- src/main/java/com/salonnipuni/api/dto/UserRequestDTO.java
+++ src/main/java/com/salonnipuni/api/dto/UserRequestDTO.java
@@ -1,4 +1,26 @@
 package com.salonnipuni.api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequestDTO {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Role is required")
+    private String role;
+
+    private String password;
 }
