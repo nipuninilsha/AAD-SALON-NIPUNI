ce491b655666955786abe477ce1e3538b204c5c5
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:50:57 +0530

    feat(dto): add ExpenseReportLineDTO class

--- src/main/java/com/salonnipuni/api/dto/ExpenseReportLineDTO.java
+++ src/main/java/com/salonnipuni/api/dto/ExpenseReportLineDTO.java
@@ -1,4 +1,16 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExpenseReportLineDTO {
+    private String date;
+    private String category;
+    private String description;
+    private double amount;
+    private String source;
 }
