2cf52d2a928073f87d00d4560397214114cb5f34
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:47:02 +0530

    feat(dto): add AuthResponseDTO class

--- src/main/java/com/salonnipuni/api/dto/AuthResponseDTO.java
+++ src/main/java/com/salonnipuni/api/dto/AuthResponseDTO.java
@@ -1,4 +1,20 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponseDTO {
+    private String id;
+    private String username;
+    private String name;
+    private String role;
+    private String email;
+    private boolean active;
+    private String token;
+    private boolean forcePasswordChange;
+    private boolean otpRequired;
 }
