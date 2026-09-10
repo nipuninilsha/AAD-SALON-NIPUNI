0fa0531a4746f0f348a596d66ced5e2013374432
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:47:37 +0530

    feat(dto): add BookingDTO class

--- src/main/java/com/salonnipuni/api/dto/BookingDTO.java
+++ src/main/java/com/salonnipuni/api/dto/BookingDTO.java
@@ -1,4 +1,27 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingDTO {
+    private String id;
+    private long bookingNumber;
+    private String clientId;
+    private String clientName;
+    private LocalDate date;
+    private String time;
+    private String status;
+    private List<BookingItemDTO> salonServices;
+    private double subtotal;
+    private double tax;
+    private double discount;
+    private double total;
+    private String paymentMethod;
 }
