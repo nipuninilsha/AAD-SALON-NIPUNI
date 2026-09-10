b13c48fac793bd89dacdd7fd8d775ef797ba7b7a
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:49:49 +0530

    feat(dto): add ChatRequestDTO class

--- src/main/java/com/salonnipuni/api/dto/ChatRequestDTO.java
+++ src/main/java/com/salonnipuni/api/dto/ChatRequestDTO.java
@@ -1,4 +1,18 @@
 package com.salonnipuni.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatRequestDTO {
+
+    @NotBlank(message = "Message is required")
+    private String message;
+
+
+    private String sessionId;
 }
