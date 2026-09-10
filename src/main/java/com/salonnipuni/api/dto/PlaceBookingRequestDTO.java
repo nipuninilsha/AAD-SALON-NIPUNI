070074696084ad40bd8e7a5a53b047d3747af3e8
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:52:56 +0530

    feat(dto): add PlaceBookingRequestDTO class

--- src/main/java/com/salonnipuni/api/dto/PlaceBookingRequestDTO.java
+++ src/main/java/com/salonnipuni/api/dto/PlaceBookingRequestDTO.java
@@ -1,4 +1,27 @@
 package com.salonnipuni.api.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlaceBookingRequestDTO {
+    @NotBlank(message = "Client is required")
+    private String clientId;
+
+    @NotEmpty(message = "At least one service or package must be added to the booking")
+    @Valid
+    private List<BookingItemRequestDTO> salonServices;
+
+    private Double discount;
+
+    @NotBlank(message = "Payment method is required")
+    private String paymentMethod;
 }
