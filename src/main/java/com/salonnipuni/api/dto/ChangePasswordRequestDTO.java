b4e9c973b062e4ce9c53e6c17b57a0a42eb1b7a1
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:49:35 +0530

    feat(dto): add ChangePasswordRequestDTO class

--- src/main/java/com/salonnipuni/api/dto/ChangePasswordRequestDTO.java
+++ src/main/java/com/salonnipuni/api/dto/ChangePasswordRequestDTO.java
@@ -1,4 +1,19 @@
 package com.salonnipuni.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChangePasswordRequestDTO {
+    @NotBlank(message = "Old password is required")
+    private String oldPassword;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 4, message = "New password must be at least 4 characters")
+    private String newPassword;
 }
