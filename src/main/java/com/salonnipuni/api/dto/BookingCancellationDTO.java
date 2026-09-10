1f23864d29d34bad1bb895010ebae7e0db4f4954
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:47:21 +0530

    feat(dto): add BookingCancellationDTO class

--- src/main/java/com/salonnipuni/api/dto/BookingCancellationDTO.java
+++ src/main/java/com/salonnipuni/api/dto/BookingCancellationDTO.java
@@ -1,4 +1,21 @@
 package com.salonnipuni.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingCancellationDTO {
+    private String id;
+    private String bookingId;
+    private String clientId;
+    private String reason;
+    private LocalDate date;
+    private List<CancellationItemDTO> salonServices;
+    private double totalRefund;
 }
