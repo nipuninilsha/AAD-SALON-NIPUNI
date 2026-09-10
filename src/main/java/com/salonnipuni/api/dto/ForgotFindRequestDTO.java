61a0c8679f6ca16975911b88f5bcef92717acfc3
nipuninilsha <nipuni.munasinghe101@gmail.com>
2026-09-10 16:51:12 +0530

    feat(dto): add ForgotFindRequestDTO class

--- src/main/java/com/salonnipuni/api/dto/ForgotFindRequestDTO.java
+++ src/main/java/com/salonnipuni/api/dto/ForgotFindRequestDTO.java
@@ -1,4 +1,14 @@
 package com.salonnipuni.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ForgotFindRequestDTO {
+    @NotBlank(message = "Username is required")
+    private String username;
 }
